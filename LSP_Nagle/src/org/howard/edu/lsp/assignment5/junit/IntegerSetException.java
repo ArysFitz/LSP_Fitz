4e12323892062ec2fa0af36d86f59a90d0f4e097
Arys <arys.fitz@gmail.com>
2026-09-23 14:52:03 +1200

    Rename project folder

--- LSP_Nagle/src/org/howard/edu/lsp/assignment5/junit/IntegerSetException.java
+++
@@ -1,5 +0,0 @@
-package org.howard.edu.lsp.assignment5.junit;
-
-public class IntegerSetException extends Exception {
-
-}
--- LSP_Nagle/src/org/howard/edu/lsp/assignment7/TestingClass.java
+++
@@ -1,5 +0,0 @@
-package org.howard.edu.lsp.assignment7;
-
-public class TestingClass {
-
-}
